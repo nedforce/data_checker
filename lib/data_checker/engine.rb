74eb4144972552a23fd62526233a1e0084c0a859
Reinier de Lange <r.j.delange@nedforce.nl>
2016-09-05 15:01:22 +0200

    Set UserAgent header to fix HTTP 999 linked_in errors

--- lib/data_checker/engine.rb
+++ lib/data_checker/engine.rb
@@ -3,10 +3,11 @@ module DataChecker
     config.data_checkers = Set.new
     config.checker_logger = DataChecker::RailsLogger
     config.link_checker_site_url = 'http://example.org'
-    
+    config.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'
+
     register_data_checkers
   end
-  
+
   def self.config; DataChecker::Engine.config; end
-  def self.logger; @logger ||= DataChecker.config.checker_logger.new; end 
+  def self.logger; @logger ||= DataChecker.config.checker_logger.new; end
 end
